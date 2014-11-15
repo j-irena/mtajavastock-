985a78d30a7fb8923f151b9868cf770d990fb5e0
irenaja <ira_1892@hotmail.com>
2014-11-15 15:00:47 +0200

    Homework #2

--- src/com/mta/javacourse/StartServlet.java
+++ src/com/mta/javacourse/StartServlet.java
@@ -1,6 +1,7 @@
 package com.mta.javacourse;
 
 import java.io.IOException;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
@@ -11,14 +12,26 @@ public class StartServlet extends HttpServlet {
 		int num1;
 		int num2;
 		int num3;
+		int radius = 50;
+		double angelB = 0.523598776;
 		
+		// Homework #1
 		num1 = 4;
 		num2 = 3;
 		num3 = 7;
-	
+		
 		int result = (num1 + num2) * num3;
 		
 		String resultStr = new String("<h1>Result of "+ "(" + num1 + "+" + num2 + ")" +"*"+num3+"="+result+"</h1>");
 		resp.getWriter().println(resultStr);
+		
+		// Homework #2
+		
+		String line1 = new String("calculation 1: Area of circle with radius " + radius + " is " + Math.PI*Math.pow(radius, 2) + " square-cm." );
+		String line2 = new String("calculation 2: Length of opposite where angle B is 30 degrees and Hypotenuse length is 50 cm is: " + 50*Math.sin(angelB) + " cm");
+		String line3 = new String("calculation 3: Power of 20 with exp of 13 is " + Math.pow(20, 13));
+		
+		String resultStr2 = line1 + "<br>" + line2 + "<br>" +line3;
+		resp.getWriter().println(resultStr2);
 	}
 }
