103fed3be2f83baafbdde266937dddb9a24b8b14
irenaja <ira_1892@hotmail.com>
2014-11-23 19:10:13 +0200

    First stock commit

--- src/com/mta/javacourse/StartServlet.java
+++ src/com/mta/javacourse/StartServlet.java
@@ -2,20 +2,23 @@ package com.mta.javacourse;
 
 import java.io.IOException;
 
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class StartServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/html");
+		
 		int num1;
 		int num2;
 		int num3;
 		int radius = 50;
 		double angelB = 0.523598776;
 		
-		// Homework #1
+		// Homework #2
 		num1 = 4;
 		num2 = 3;
 		num3 = 7;
@@ -25,7 +28,7 @@ public class StartServlet extends HttpServlet {
 		String resultStr = new String("<h1>Result of " + "(" + num1 + " + " + num2 + ")" + " * " + num3 + " = " + result + "</h1>");
 		resp.getWriter().println(resultStr);
 		
-		// Homework #2
+		// Homework #3
 		
 		String line1 = new String("calculation 1: Area of circle with radius " + radius + " is " + Math.PI * Math.pow(radius, 2) + " square-cm." );
 		String line2 = new String("calculation 2: Length of opposite where angle B is 30 degrees and Hypotenuse length is 50 cm is: " + 50 * Math.sin(angelB) + " cm");
--- src/com/mta/javacourse/Stock.java
+++ src/com/mta/javacourse/Stock.java
@@ -1,7 +1,6 @@
 package com.mta.javacourse;
-
+ 
 import java.io.IOException;
-import java.util.Date;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,24 +17,31 @@ public class Stock extends HttpServlet {
 	public String getSymbol() {
 		return symbol;
 	}
+	
 	public void setSymbol(String symbol) {
 		this.symbol = symbol;
 	}
+	
 	public float getAsk() {
 		return ask;
 	}
+	
 	public void setAsk(float ask) {
 		this.ask = ask;
 	}
+	
 	public float getBid() {
 		return bid;
 	}
+	
 	public void setBid(float bid) {
 		this.bid = bid;
 	}
+	
 	public java.util.Date getDate() {
 		return date;
 	}
+	
 	public void setDate(java.util.Date date) {
 		this.date = date;
 	}
@@ -43,29 +49,5 @@ public class Stock extends HttpServlet {
 	public String getHtmlDescription() {
 		htmlDescription  = "<b>Stock symbol is </b>" + getSymbol() + ",<b> ask: </b>" + getAsk() + ",<b> bid: </b>" + getBid() + ",<b> date: </b>" + getDate();
 		return htmlDescription;
-	}
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		
-	Stock st1 = new Stock();
-	st1.setSymbol("PIH");
-	st1.setAsk((float)( 12.4));
-	st1.setBid((float)(13.1));
-	st1.setDate(new Date());
-	
-	Stock st2 = new Stock();
-	st2.setSymbol("AAL");
-	st2.setAsk((float)(5.5));
-	st2.setBid((float)(5.78));
-	st2.setDate(new Date());
-	
-	Stock st3 = new Stock();
-	st3.setSymbol("CAAS");
-	st3.setAsk((float)(31.5));
-	st3.setBid((float)(31.2));
-	st3.setDate(new Date());
-	
-	resp.getWriter().println(st1.getHtmlDescription() + "<br>" + st2.getHtmlDescription() + "<br>" + st3.getHtmlDescription());
-	resp.setContentType("text/html");
 	}
 }
